b05684154ce3918ca0fee9d4abbfa687f645deb4
Taras Hupalo <taras.hupalo@gmail.com>
2016-03-24 15:14:30 +0200

    Implement fake email generator

--- app/mail/mailEditor.js
+++ app/mail/mailEditor.js
@@ -7,12 +7,17 @@ angular.module('app').component('mailEditor', {
     onCreate: '&',
     onCancel: '&'
   },
-  controller: function() {
+  controller: function(ContactsService) {
     this.mail = {};
 
-    this.submit = () => {
-      this.onCreate({mail: this.mail});
+    this.submit = () => this.onCreate({mail: this.mail});
 
+    this.fakeEmail = () => {
+      ContactsService.generateFake().then(fake => {
+        this.mail.to = fake.email;
+        this.mail.subject = faker.directive('lorem')('%w', 3, 3);
+        this.mail.text = faker.directive('lorem')('%p', 3, 3);
+      });
     }
   }
 });
