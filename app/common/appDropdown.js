eefb019f66785e0638c9da03fac77056a6995f75
Taras Hupalo <taras.hupalo@gmail.com>
2016-03-24 14:58:53 +0200

    Fix app toggling

--- app/common/appDropdown.js
+++ app/common/appDropdown.js
@@ -2,14 +2,12 @@
 
 angular.module('app').component('appDropdown', {
   templateUrl: 'app/common/appDropdown.html',
+  bindings: {
+    currentApp: '@'
+  },
   controller: function($scope) {
     this.selectApp = (app) => {
-      this.currentApp = app;
       $scope.$emit('change-app', app);
     };
-
-    $scope.$on('change-app', (e, app) => {
-      this.currentApp = app;
-    })
   }
 });
