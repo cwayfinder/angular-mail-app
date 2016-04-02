9d997ad6122264130501eae58a9ff51b366e5595
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-02 19:35:38 +0300

    Restore the selected folder highlighting

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,14 @@ angular.module('app').config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/mailbox/folder/inbox')
 
+  var mailboxAsideView = {
+    template: `<mails-sidebar selected-folder="$ctrl.folder"></mails-sidebar>`,
+    controller: function($stateParams) {
+      this.folder = $stateParams.folder;
+    },
+    controllerAs: '$ctrl'
+  };
+
   $stateProvider
     .state('app', {
       abstract: true,
@@ -37,37 +45,52 @@ angular.module('app').config(function($stateProvider, $urlRouterProvider) {
 
     .state('app.mailbox.compose', {
       url: '/compose',
-      template: `<mail-editor></mail-editor>`
+      views: {
+        main: {
+          template: `<mail-editor></mail-editor>`
+        },
+        aside: mailboxAsideView
+      }
     })
 
     .state('app.mailbox.mail', {
       url: '/mail/:mailId',
-      template: `<mail-panel mail="$ctrl.mail"></mail-panel>`,
-      resolve: {
-        mail: function($stateParams, MailService, currentUser) {
-          return MailService.getMail($stateParams.mailId, currentUser);
-        }
-      },
-      controller: function($stateParams, mail) {
-        this.mail = mail;
-      },
-      controllerAs: '$ctrl'
+      views: {
+        main: {
+          template: `<mail-panel mail="$ctrl.mail"></mail-panel>`,
+          resolve: {
+            mail: function($stateParams, MailService, currentUser) {
+              return MailService.getMail($stateParams.mailId, currentUser);
+            }
+          },
+          controller: function($stateParams, mail) {
+            this.mail = mail;
+          },
+          controllerAs: '$ctrl'
+        },
+        aside: mailboxAsideView
+      }
     })
 
     .state('app.mailbox.folder', {
       url: '/folder/:folder',
-      template: `<mail-list mails="$ctrl.mails" folder="{{$ctrl.folder}}"></mail-list>`,
-      resolve: {
-        mails: function($stateParams, MailService, UserService, currentUser) {
-          let methodName = 'get' + capitalize($stateParams.folder);
-          return MailService[methodName](UserService.getCurrentUser());
-        }
-      },
-      controller: function($stateParams, mails) {
-        this.mails = mails;
-        this.folder = $stateParams.folder;
-      },
-      controllerAs: '$ctrl'
+      views: {
+        main: {
+          template: `<mail-list mails="$ctrl.mails" folder="{{$ctrl.folder}}"></mail-list>`,
+          resolve: {
+            mails: function($stateParams, MailService, UserService, currentUser) {
+              let methodName = 'get' + capitalize($stateParams.folder);
+              return MailService[methodName](UserService.getCurrentUser());
+            }
+          },
+          controller: function($stateParams, mails) {
+            this.mails = mails;
+            this.folder = $stateParams.folder;
+          },
+          controllerAs: '$ctrl'
+        },
+        aside: mailboxAsideView
+      }
     })
 
     .state('app.contacts', {
